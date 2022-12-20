d81a56cf0c80f5597d1a1fc09c6f54c488a46d89
Igor Bukur <tuturox91@gmail.com>
2022-12-20 21:18:20 +0200

    Finish

--- src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
+++ src/main/java/mate/academy/spring/dto/request/UserRequestDto.java
@@ -1,7 +1,18 @@
 package mate.academy.spring.dto.request;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import mate.academy.spring.validation.Email;
+
 public class UserRequestDto {
+    @NotNull
+    @NotBlank(message = " field can't be empty")
+    @Email
     private String email;
+    @NotNull
+    @Size(min = 7)
+    @NotBlank(message = " field can't be empty")
     private String password;
 
     public String getEmail() {
